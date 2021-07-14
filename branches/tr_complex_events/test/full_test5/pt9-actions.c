fbb495c3edcad5ae839cc435d7c0c56c9fed50cb
sstanton <sstanton@cf6c6029-4112-4f43-97e6-ba72ee8e0d75>
2021-07-14 03:52:47 +0000

    Continuing the restoration starting in revision 9, this branch was found in the checkout on ThreeBagshotRow.  Revision 11 will be the updated code found in that branch on ThreeBagshotRow.

---
+++ branches/tr_complex_events/test/full_test5/pt9-actions.c
@@ -0,0 +1,45 @@
+#include "parser-test9.h"
+
+void newMachine_a2(pNEW_MACHINE pfsm)
+{
+   (void) pfsm;
+
+   printf("newMachine_a2\n");
+}
+
+void newMachine_a1(pNEW_MACHINE pfsm)
+{
+   (void) pfsm;
+
+   printf("newMachine_a1\n");
+}
+
+void newMachine_noAction(pNEW_MACHINE pfsm)
+{
+   (void) pfsm;
+
+   printf("newMachine_noAction\n");
+}
+
+NEW_MACHINE_STATE newMachine_transitionFn(pNEW_MACHINE pfsm, NEW_MACHINE_EVENT e)
+{
+   (void) pfsm;
+   (void) e;
+
+   return newMachine_s1;
+}
+
+NEW_MACHINE_STATE newMachine_transitionFn1(pNEW_MACHINE pfsm, NEW_MACHINE_EVENT e)
+{
+   (void) pfsm;
+   (void) e;
+
+   return newMachine_s1;
+}
+
+int main(int argc, char **argv)
+{
+   printf("%s: hello, world\n", argv[0]);
+
+   return 0;
+}
---
+++ branches/tr_complex_events/test/test.mk
@@ -0,0 +1,31 @@
+#######################################
+#
+# Generic rules for creating and running tests.
+#
+#
+
+include ../../depends.mk
+
+OBJS = $(SRC:.c=.o) $(FSM_SRC:.fsm=.o)
+
+ifndef NO_RUNTEST
+runtest: test
+	@./test > test.out
+	@diff test.out test.canonical > test.result
+	@echo "Test successful"
+	@rm test.out test.result
+endif
+
+test: $(OBJS)
+	$(CC) -o $@ $(OBJS) $(LDFLAGS)
+
+clean::
+	-@rm *.exe 2> /dev/nul
+	-@rm *.out 2> /dev/nul
+	-@rm *.result 2> /dev/nul
+	-@rm *.d* 2> /dev/nul
+	-@rm *.o 2> /dev/nul
+	-@rm *.stackdump 2> /dev/nul
+	-@rm $(FSM_SRC:.fsm=.c) 2> /dev/nul
+	-@rm $(FSM_SRC:.fsm=.h) 2> /dev/nul
+
