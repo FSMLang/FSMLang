db2c2d7a890e24cc171dbecc531bcecd87442a81
ssinsulet <ssinsulet@cf6c6029-4112-4f43-97e6-ba72ee8e0d75>
2021-08-24 21:36:33 +0000

    Create html tests.

--- branches/html_css/fsmrules.mk
+++ branches/html_css/fsmrules.mk
@@ -3,31 +3,38 @@
 #  .fsm rules
 #
 
-.SUFFIXES: .fsm 
+.SUFFIXES: .fsm .html
 
 ifdef OUTPUT_DIR
-FSM=$(OUTPUT_DIR)/fsm $(FSM_FLAGS)
+FSM=$(OUTPUT_DIR)/fsm
 endif
 
-FSM ?= fsm $(FSM_FLAGS)
+FSM ?= fsm
 
 .fsm.o:
-	@echo "FSM:" $(FSM)
-	$(FSM) $< > fsmout
+	@echo "FSM: " $(FSM)
+	$(FSM) $(FSM_FLAGS) $< > fsmout
 	$(CC) -c $(CFLAGS) $*.c
 	rm $*.c
 
 .fsm.c:
-	@echo "FSM:" $(FSM)
-	$(FSM) $< > fsmout
+	@echo "FSM: " $(FSM)
+	$(FSM) $(FSM_FLAGS) $< > fsmout
 
 .fsm.h:
-	@echo "FSM:" $(FSM)
-	$(FSM) $< > fsmout
+	@echo "FSM: " $(FSM)
+	$(FSM) $(FSM_FLAGS) $< > fsmout
 
-$(FSM_SRC:.fsm=.h): $(FSM_SRC)
+.fsm.html:
+	@echo "FSM: " $(FSM)
+	$(FSM) -th $(FSM_HTML_FLAGS) $< 
 
-$(FSM_SRC:.fsm=.c): $(FSM_SRC)
+
+$(FSM_SRC:.fsm=.h)   : $(FSM_SRC)
+
+$(FSM_SRC:.fsm=.c)   : $(FSM_SRC)
+
+$(FSM_SRC:.fsm=.html): $(FSM_SRC)
 
 $(SRC): $(FSM_SRC:.fsm=.h)
 
