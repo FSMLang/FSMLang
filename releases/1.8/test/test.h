9e7cf25d55af697cdecf56dd94af66d6960ab199
ssinsulet <ssinsulet@cf6c6029-4112-4f43-97e6-ba72ee8e0d75>
2022-07-24 02:51:36 +0000

    Release 1.8 - Hierarchical state machines.

---
+++ releases/1.8/test/test.h
@@ -0,0 +1,11 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <stdio.h>
+
+#define DBG_PRINTF(...) printf(__VA_ARGS__)
+
+#define FSM_END_CRITICAL printf("end state: %s\n\n",TEST_FSM_STATE_NAMES[pfsm->state]);
+
+
+#endif /* --------------------------  Nothing below  ----------------------- */
---
+++ releases/1.8/test/unit/unitTest.mk
@@ -0,0 +1,16 @@
+VPATH=../../../src
+
+override CFLAGS += -I../../../src -DARCH=$(ARCH)
+
+SRC+=$(shell find -L . -type f -name "*.c" -printf "%P ")
+
+OBJ=$(SRC:.c=.o)
+
+clean:
+	-rm -f $(OBJ) test
+
+test: $(OBJ)
+	gcc -o $@ $(OBJ)
+
+runtest: test
+	./test
