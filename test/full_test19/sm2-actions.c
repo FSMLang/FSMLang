a3b9913dea4828864ffc8abd1c0cd33966d37d48
ssinsulet <101585249+9Fingered@users.noreply.github.com>
2022-08-05 20:48:08 +0000

    All FSM build rule targets depend on the fsm executable being used. Pesky extraneous underbar removed. Idea of "current sub-machine" removed; replaced by lookup based on current event. Sub-machines now have an enumeration. Event, state, and sub-machine enumerations have a counter element at the end. This is an interim commit; more to follow.

--- test/full_test19/sm2-actions.c
+++ test/full_test19/sm2-actions.c
@@ -1,27 +1,27 @@
 #include "sub_machine2.h"
 
-TOP__LEVEL_EVENT sub_machine2_a3(pSUB__MACHINE2 pfsm)
+TOP_LEVEL_EVENT sub_machine2_a3(pSUB_MACHINE2 pfsm)
 {
    printf("sub_machine2_a3\n");
 
    return top_level_e4;
 }
 
-TOP__LEVEL_EVENT sub_machine2_a2(pSUB__MACHINE2 pfsm)
+TOP_LEVEL_EVENT sub_machine2_a2(pSUB_MACHINE2 pfsm)
 {
    printf("sub_machine2_a2\n");
 
    return sub_machine2_e3;
 }
 
-TOP__LEVEL_EVENT sub_machine2_a1(pSUB__MACHINE2 pfsm)
+TOP_LEVEL_EVENT sub_machine2_a1(pSUB_MACHINE2 pfsm)
 {
    printf("sub_machine2_a1\n");
 
    return sub_machine2_e2;
 }
 
-TOP__LEVEL_EVENT sub_machine2_noAction(pSUB__MACHINE2 pfsm)
+TOP_LEVEL_EVENT sub_machine2_noAction(pSUB_MACHINE2 pfsm)
 {
    printf("sub_machine2_noAction\n");
 
--- test/full_test19/sm3-actions.c
+++ test/full_test19/sm3-actions.c
@@ -1,29 +1,29 @@
 #include "sub_machine3.h"
 
-TOP__LEVEL_EVENT sub_machine3_a3(pSUB__MACHINE3 pfsm)
+TOP_LEVEL_EVENT sub_machine3_a3(pSUB_MACHINE3 pfsm)
 {
-   printf("sub_machine3_a3\n");
+   DBG_PRINTF("sub_machine3_a3\n");
 
    return top_level_noEvent;
 }
 
-TOP__LEVEL_EVENT sub_machine3_a2(pSUB__MACHINE3 pfsm)
+TOP_LEVEL_EVENT sub_machine3_a2(pSUB_MACHINE3 pfsm)
 {
-   printf("sub_machine3_a2\n");
+   DBG_PRINTF("sub_machine3_a2\n");
 
    return sub_machine3_e3;
 }
 
-TOP__LEVEL_EVENT sub_machine3_a1(pSUB__MACHINE3 pfsm)
+TOP_LEVEL_EVENT sub_machine3_a1(pSUB_MACHINE3 pfsm)
 {
-   printf("sub_machine3_a1\n");
+   DBG_PRINTF("sub_machine3_a1\n");
 
    return sub_machine3_e2;
 }
 
-TOP__LEVEL_EVENT sub_machine3_noAction(pSUB__MACHINE3 pfsm)
+TOP_LEVEL_EVENT sub_machine3_noAction(pSUB_MACHINE3 pfsm)
 {
-   printf("sub_machine3_noAction\n");
+   DBG_PRINTF("sub_machine3_noAction\n");
 
    return sub_machine3_noEvent;
 }
