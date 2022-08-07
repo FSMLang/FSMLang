a783e164a274ccb462fba482ee68c8f7f93d455e
ssinsulet <ssinsulet@cf6c6029-4112-4f43-97e6-ba72ee8e0d75>
2022-08-07 09:07:07 +0000

    rebase trunk picks up fsmrules.mk fix. canonical test output updated. flashdriver.fsm defines DBG_PRINTF

--- branches/concurrent_sub_machines/fsmrules.mk
+++ branches/concurrent_sub_machines/fsmrules.mk
@@ -6,28 +6,28 @@
 .SUFFIXES: .fsm .html
 
 ifdef OUTPUT_DIR
-FSM=$(OUTPUT_DIR)/fsm $(FSM_FLAGS)
+FSM=$(OUTPUT_DIR)/fsm
 endif
 
-FSM ?= fsm $(FSM_FLAGS)
+FSM ?= fsm
 
 .fsm.o:
-	@echo "FSM:" $(FSM)
+	@echo "FSM:" $(FSM) $(FSM_FLAGS)
 	$(FSM) $< > fsmout
 	$(CC) -c $(CFLAGS) $*.c
 	rm $*.c
 
 .fsm.c:
 	@echo "FSM:" $(FSM)
-	$(FSM) $< > fsmout
+	$(FSM) $(FSM_FLAGS) $< > fsmout
 
 .fsm.h:
 	@echo "FSM:" $(FSM)
-	$(FSM) $< > fsmout
+	$(FSM) $(FSM_FLAGS) $< > fsmout
 
 .fsm.html:
 	@echo "FSM:" $(FSM)
-	$(FSM) -th $< > fsmout
+	$(FSM) $(FSM_FLAGS) -th $< > fsmout
 
 $(FSM_SRC:.fsm=.html): $(FSM_SRC) $(FSM)
 
