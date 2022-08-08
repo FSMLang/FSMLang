9d321e91a5ffc5203b73b7041bb51a99ff271dde
ssinsulet <ssinsulet@cf6c6029-4112-4f43-97e6-ba72ee8e0d75>
2022-08-08 01:45:46 +0000

    merge ~/branches/concurrent_sub_machines.

--- trunk/test/full_test19/tl-actions.c
+++ trunk/test/full_test19/tl-actions.c
@@ -2,39 +2,22 @@
 
 #include "top_level.h"
 
-TOP_LEVEL_EVENT top_level_a1(pTOP_LEVEL pfsm)
-{
-   DBG_PRINTF("top_level_a1\n");
-
-   return top_level_noEvent;
-}
-
 TOP_LEVEL_EVENT top_level_activate_sub_machine1(pTOP_LEVEL pfsm)
 {
-   DBG_PRINTF("top_level_activate_sub_machine1\n");
-
-   return top_level_sub_machine1_e1;
+	DBG_PRINTF("top_level_activate_sub_machine1\n");
+	return top_level_sub_machine1_e1;
 }
 
 TOP_LEVEL_EVENT top_level_activate_sub_machine2(pTOP_LEVEL pfsm)
 {
-   DBG_PRINTF("top_level_activate_sub_machine2\n");
-
-   return top_level_sub_machine2_e1;
+	DBG_PRINTF("top_level_activate_sub_machine2\n");
+	return top_level_sub_machine2_e1;
 }
 
 TOP_LEVEL_EVENT top_level_activate_sub_machine3(pTOP_LEVEL pfsm)
 {
-   DBG_PRINTF("top_level_activate_sub_machine3\n");
-
-   return top_level_sub_machine3_e1;
-}
-
-TOP_LEVEL_EVENT top_level_noAction(pTOP_LEVEL pfsm)
-{
-   DBG_PRINTF("top_level_noAction\n");
-
-   return top_level_noEvent;
+	DBG_PRINTF("top_level_activate_sub_machine3\n");
+	return top_level_sub_machine3_e1;
 }
 
 int main (int argc, char **argv)
