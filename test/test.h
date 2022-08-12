f5b88f6f59bed54b2897335868e1be261d89631c
ssinsulet <101585249+9Fingered@users.noreply.github.com>
2022-08-12 21:21:37 +0000

    rebase to trunk

--- test/test.h
+++ test/test.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-#define DBG_PRINTF(...) printf(__VA_ARGS__)
+#define DBG_PRINTF(...) printf(__VA_ARGS__); printf("\n")
 
 #define FSM_END_CRITICAL printf("end state: %s\n\n",TEST_FSM_STATE_NAMES[pfsm->state]);
 
