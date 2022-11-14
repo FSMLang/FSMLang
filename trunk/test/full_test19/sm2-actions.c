e9a20447d65fd0fc9f3dd33eb106e40ea77d2884
ringwinner <ringwinner@cf6c6029-4112-4f43-97e6-ba72ee8e0d75>
2022-11-14 18:55:28 +0000

    commit merge from ~/branches/hsm_event_enumerations

--- trunk/test/full_test19/sm2-actions.c
+++ trunk/test/full_test19/sm2-actions.c
@@ -4,27 +4,27 @@ TOP_LEVEL_EVENT sub_machine2_a3(pSUB_MACHINE2 pfsm)
 {
    printf("sub_machine2_a3\n");
 
-   return top_level_e4;
+   return PARENT(e4);
 }
 
 TOP_LEVEL_EVENT sub_machine2_a2(pSUB_MACHINE2 pfsm)
 {
    printf("sub_machine2_a2\n");
 
-   return sub_machine2_e3;
+   return THIS(e3);
 }
 
 TOP_LEVEL_EVENT sub_machine2_a1(pSUB_MACHINE2 pfsm)
 {
    printf("sub_machine2_a1\n");
 
-   return sub_machine2_e2;
+   return THIS(e2);
 }
 
 TOP_LEVEL_EVENT sub_machine2_noAction(pSUB_MACHINE2 pfsm)
 {
    printf("sub_machine2_noAction\n");
 
-   return sub_machine2_noEvent;
+   return THIS(noEvent);
 }
 
--- trunk/test/full_test26/sm1-actions.c
+++ trunk/test/full_test26/sm1-actions.c
@@ -4,31 +4,31 @@ TOP_LEVEL_EVENT sub_machine1_a3(pSUB_MACHINE1 pfsm)
 {
    DBG_PRINTF("sub_machine1_a3\n");
 
-   return top_level_e3;
+   return PARENT(e3);
 }
 
 TOP_LEVEL_EVENT sub_machine1_a2(pSUB_MACHINE1 pfsm)
 {
    DBG_PRINTF("sub_machine1_a2\n");
 
-   return sub_machine1_e3;
+   return THIS(e3);
 }
 
 TOP_LEVEL_EVENT sub_machine1_a1(pSUB_MACHINE1 pfsm)
 {
    DBG_PRINTF("sub_machine1_a1\n");
 
-   return sub_machine1_e2;
+   return THIS(e2);
 }
 
 TOP_LEVEL_EVENT sub_machine1_noAction(pSUB_MACHINE1 pfsm)
 {
    DBG_PRINTF("sub_machine1_noAction\n");
 
-   return sub_machine1_noEvent;
+   return THIS(noEvent);
 }
 
-SUB_MACHINE1_STATE sub_machine1_checkTransition(pSUB_MACHINE1 pfsm, SUB_MACHINE1_EVENT e)
+SUB_MACHINE1_STATE sub_machine1_checkTransition(pSUB_MACHINE1 pfsm, TOP_LEVEL_EVENT e)
 {
    (void) pfsm;
    (void) e;
