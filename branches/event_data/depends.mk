c77d8451843b575df0109be3ac86dd5590a1ddd7
ringwinner <ringwinner@cf6c6029-4112-4f43-97e6-ba72ee8e0d75>
2022-12-11 13:55:07 +0000

    understand the machine's data block. allow events to specify that they need data. perhaps create helper functions which accept data of the right type and call the FSM. perhaps create lexer/parser which will test the machine.

---
+++ branches/event_data/depends.mk
@@ -0,0 +1,13 @@
+%.d: %.c
+	@set -e; rm -f $@; \
+	$(CC) -M $(CFLAGS) $< > $@.$$$$; \
+	sed 's,\($*\)\.o[ :]*,\1.o $@ : Makefile ,g' < $@.$$$$ > $@; \
+	rm -f $@.$$$$
+
+%.i: %.c
+	$(CC) -E $(CFLAGS) $< > $@
+
+ifneq ($(MAKECMDGOALS),clean)
+-include $(SRC:.c=.d)
+endif
+
