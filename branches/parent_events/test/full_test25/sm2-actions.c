a3005f33df6a40f7e6d59c9608f9dac7f57c912a
ringwinner <ringwinner@cf6c6029-4112-4f43-97e6-ba72ee8e0d75>
2022-12-18 14:28:57 +0000

    sub-machines designate events as stemming from the parent.  When the event occurs, the parent handles it (before, after?) passing it to the sub-machine.

---
+++ branches/parent_events/test/full_test25/sm2-actions.c
@@ -0,0 +1,30 @@
+#include "sub_machine2.h"
+
+TOP_LEVEL_EVENT sub_machine2_a3(pSUB_MACHINE2 pfsm)
+{
+   printf("sub_machine2_a3\n");
+
+   return PARENT(e4);
+}
+
+TOP_LEVEL_EVENT sub_machine2_a2(pSUB_MACHINE2 pfsm)
+{
+   printf("sub_machine2_a2\n");
+
+   return THIS(e3);
+}
+
+TOP_LEVEL_EVENT sub_machine2_a1(pSUB_MACHINE2 pfsm)
+{
+   printf("sub_machine2_a1\n");
+
+   return THIS(e2);
+}
+
+TOP_LEVEL_EVENT sub_machine2_noAction(pSUB_MACHINE2 pfsm)
+{
+   printf("sub_machine2_noAction\n");
+
+   return THIS(noEvent);
+}
+
