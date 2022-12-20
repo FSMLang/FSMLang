44b312c0dbf2963317088575ba258f8c0207dbc3
ringwinner <ringwinner@cf6c6029-4112-4f43-97e6-ba72ee8e0d75>
2022-12-20 04:04:41 +0000

    bump rev 1.26

--- trunk/src/revision.c
+++ trunk/src/revision.c
@@ -186,8 +186,9 @@
 */
 
 /*
-   Revision 1.25:
+   Revision 1.26:
  
    RUN_STATE_MACHINE and DECLARE_xxx_MACHINE comply with MISRA C:2012:20.7
 */
-const char rev_string[] = "1.25_E1";
+const char rev_string[] = "1.26";
+
