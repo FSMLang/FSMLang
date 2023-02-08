e0d1f834211a5b4fc14e5fd470c4924fc8afdb2d
djasinski@insulet.com <djasinski@insulet.com@cf6c6029-4112-4f43-97e6-ba72ee8e0d75>
2023-02-08 21:32:31 +0000

    Create branch for file path handling

---
+++ branches/filepath_handling/test/full_test6/pt11-actions.c
@@ -0,0 +1,66 @@
+#include "parser-test11.h"
+
+NEW_MACHINE_EVENT newMachine_a2(pNEW_MACHINE pfsm)
+{
+   (void) pfsm;
+   DBG_PRINTF("newMachine_a2\n");
+
+   return newMachine_noEvent;
+}
+
+NEW_MACHINE_EVENT newMachine_a1(pNEW_MACHINE pfsm)
+{
+   (void) pfsm;
+   DBG_PRINTF("newMachine_a1\n");
+
+   return newMachine_noEvent;
+}
+
+NEW_MACHINE_EVENT newMachine_noAction(pNEW_MACHINE pfsm)
+{
+   (void) pfsm;
+   DBG_PRINTF("newMachine_noAction\n");
+
+   return newMachine_noEvent;
+}
+
+void newMachine_machineTransitionFn(pNEW_MACHINE pfsm, NEW_MACHINE_STATE new_state)
+{
+   DBG_PRINTF("newMachine_machineTransitionFn\n\told state: %s\n\tnew state: %s\n"
+              , NEW_MACHINE_STATE_NAMES[pfsm->state]
+              , NEW_MACHINE_STATE_NAMES[new_state]
+              );
+}
+
+NEW_MACHINE_STATE newMachine_transitionFn(pNEW_MACHINE pfsm,NEW_MACHINE_EVENT event)
+{
+   (void) pfsm;
+   (void) event;
+
+   DBG_PRINTF("newMachine_transitionFn\n");
+
+   return newMachine_s3;
+}
+
+NEW_MACHINE_STATE newMachine_transitionFn1(pNEW_MACHINE pfsm,NEW_MACHINE_EVENT event)
+{
+   (void) pfsm;
+   (void) event;
+
+   DBG_PRINTF("newMachine_transitionFn1\n");
+
+   return newMachine_s2;
+}
+
+int main(int argc, char **argv)
+{
+   fprintf(stdout,"hello, world\n");
+
+   RUN_STATE_MACHINE(pnewMachine, newMachine_e1);
+   RUN_STATE_MACHINE(pnewMachine, newMachine_e2);
+   RUN_STATE_MACHINE(pnewMachine, newMachine_e3);
+   RUN_STATE_MACHINE(pnewMachine, newMachine_e4);
+
+   return 0;
+}
+
