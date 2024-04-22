f3bf4977360ab930d54b3256140f8f13400cd0e5
9Fingered <sstanton@pswgroup.net>
2024-04-21 23:24:33 -0400

    C Table sub-machines now descend.

---
+++ test/full_test86/sm1-actions.c
@@ -0,0 +1,42 @@
+#include "sub_machine1_priv.h"
+
+TOP_LEVEL_EVENT UFMN(a3)(pSUB_MACHINE1 pfsm)
+{
+	(void) pfsm;
+   DBG_PRINTF("%s\n", __func__);
+
+   return PARENT(e3);
+}
+
+TOP_LEVEL_EVENT UFMN(a2)(pSUB_MACHINE1 pfsm)
+{
+	(void) pfsm;
+   DBG_PRINTF("%s\n", __func__);
+
+   return THIS(e3);
+}
+
+TOP_LEVEL_EVENT UFMN(a1)(pSUB_MACHINE1 pfsm)
+{
+	(void) pfsm;
+   DBG_PRINTF("%s\n", __func__);
+
+   return THIS(e2);
+}
+
+TOP_LEVEL_EVENT UFMN(noAction)(pSUB_MACHINE1 pfsm)
+{
+	(void) pfsm;
+   DBG_PRINTF("%s\n", __func__);
+
+   return THIS(noEvent);
+}
+
+SUB_MACHINE1_STATE UFMN(checkTransition)(pSUB_MACHINE1 pfsm, TOP_LEVEL_EVENT e)
+{
+   (void) pfsm;
+   (void) e;
+
+   return sub_machine1_s3;
+}
+
---
+++ test/full_test86/sm2-actions.c
@@ -0,0 +1,34 @@
+#include "sub_machine2_priv.h"
+
+TOP_LEVEL_EVENT UFMN(a3)(pSUB_MACHINE2 pfsm)
+{
+	(void) pfsm;
+   DBG_PRINTF("%s\n", __func__);
+
+   return PARENT(e4);
+}
+
+TOP_LEVEL_EVENT UFMN(a2)(pSUB_MACHINE2 pfsm)
+{
+	(void) pfsm;
+   DBG_PRINTF("%s\n", __func__);
+
+   return THIS(e3);
+}
+
+TOP_LEVEL_EVENT UFMN(a1)(pSUB_MACHINE2 pfsm)
+{
+	(void) pfsm;
+   DBG_PRINTF("%s\n", __func__);
+
+   return THIS(e2);
+}
+
+TOP_LEVEL_EVENT UFMN(noAction)(pSUB_MACHINE2 pfsm)
+{
+	(void) pfsm;
+   DBG_PRINTF("%s\n", __func__);
+
+   return THIS(noEvent);
+}
+
