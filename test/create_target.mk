e078210026c334b276f3543d38a0d2fa5ca91790
9Fingered <sstanton@pswgroup.net>
2024-10-22 21:59:28 -0400

    allow Makefiles to add SRC.

--- test/create_target.mk
+++ test/create_target.mk
@@ -9,7 +9,7 @@ CFLAGS += -DFSM_DEBUG -DTEST_FSM_DEBUG -I../
 FSM_SRC = $(shell find . -name "*.fsm" -printf "%P ")
 
 #action functions are placed in files ending in "-actions.c"
-SRC = $(shell find . -name "*-actions.c" -printf "%P ")
+SRC += $(shell find . -name "*-actions.c" -printf "%P ")
 
 #some tests have main in one of the action fiies; others put it in test.c
 SRC += $(shell find . -name "test.c" -print)
