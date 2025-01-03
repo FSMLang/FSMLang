8b78650f7a338b65528d97394645c1220e411576
9Fingered <101585249+9Fingered@users.noreply.github.com>
2025-01-03 14:50:50 -0500

    Declare/define machine data structures in the sub machine header. (#135)

---
+++ test/full_test73/ss-actions.c
@@ -0,0 +1,21 @@
+#include "subSub_priv.h"
+#include <stddef.h>
+
+#ifndef DBG_PRINTF
+#define DBG_PRINTF(...)
+#endif
+
+TEST_EVENT __attribute__((weak)) UFMN(a1)(FSM_TYPE_PTR pfsm)
+{
+	DBG_PRINTF("weak: %s", __func__);
+	(void) pfsm;
+	return THIS(noEvent);
+}
+
+TEST_EVENT __attribute__((weak)) UFMN(noAction)(FSM_TYPE_PTR pfsm)
+{
+	DBG_PRINTF("weak: %s", __func__);
+	(void) pfsm;
+	return THIS(noEvent);
+}
+
