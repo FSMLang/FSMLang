f1ffd307076af3040ea15f8b838ff80e909a9fb2
9Fingered <sstanton@pswgroup.net>
2025-06-28 18:11:17 -0400

    First tests for event sequencs.

---
+++ src/event_sequences.h
@@ -0,0 +1,40 @@
+/**
+*  event_sequences.h
+*
+*    Diagrams event sequences
+*
+*    FSMLang (fsm) - A Finite State Machine description language.
+*    Copyright (C) 2025  Steven Stanton
+*
+*    This program is free software; you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation; either version 2 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*    Steven Stanton
+*    fsmlang@pesticidesoftware.com
+*
+*    For the latest on FSMLang: https://fsmlang.github.io
+*
+*    And, finally, your possession of this source code implies nothing.
+*
+*    File created by Steven Stanton
+*
+*  Long Description:
+*
+*/
+
+#ifndef EVENT_SEQUENCES_H
+#define EVENT_SEQUENCES_H
+
+#endif
+
