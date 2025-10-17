a02cc5fac155d6d18d561ed2f23efbc02dd70811
9Fingered <sstanton@pswgroup.net>
2025-10-17 16:18:26 -0400

    -i0 works with two sub-machines.

---
+++ test/full_test115/sub-actions.c
@@ -0,0 +1,27 @@
+
+#include "sub_priv.h"
+
+ACTION_RETURN_TYPE UFMN(do_nothing)(FSM_TYPE_PTR pfsm)
+{
+	DBG_PRINTF("%s", __func__);
+	(void) pfsm;
+}
+
+ACTION_RETURN_TYPE UFMN(noAction)(FSM_TYPE_PTR pfsm)
+{
+	DBG_PRINTF("%s", __func__);
+	(void) pfsm;
+
+}
+
+ACTION_RETURN_TYPE test_fsm_sub_grab_parent_e1_data(pTEST_FSM_DATA pdata, const void *pparent_fsm)
+{
+	DBG_PRINTF("%s", __func__);
+
+	const pSUB psub = (const pSUB) pparent_fsm;
+
+	psub->data.sub_e1_count += pdata->e1_count;
+
+	DBG_PRINTF("sub e1 accumulator: %u", psub->data.sub_e1_count);
+}
+
---
+++ test/full_test115/sub2-actions.c
@@ -0,0 +1,27 @@
+
+#include "sub2_priv.h"
+
+ACTION_RETURN_TYPE UFMN(do_nothing)(FSM_TYPE_PTR pfsm)
+{
+	DBG_PRINTF("%s", __func__);
+	(void) pfsm;
+}
+
+ACTION_RETURN_TYPE UFMN(noAction)(FSM_TYPE_PTR pfsm)
+{
+	DBG_PRINTF("%s", __func__);
+	(void) pfsm;
+
+}
+
+ACTION_RETURN_TYPE UFMN(grab_parent_e1_data)(pTEST_FSM_DATA pdata, const void *pparent_fsm)
+{
+	DBG_PRINTF("%s", __func__);
+
+	const pSUB2 psub2 = (const pSUB2) pparent_fsm;
+
+	psub2->data.sub2_e1_count += pdata->e1_count;
+
+	DBG_PRINTF("sub2 e1 accumulator: %u", psub2->data.sub2_e1_count);
+}
+
