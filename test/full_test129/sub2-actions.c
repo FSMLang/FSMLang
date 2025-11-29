739a8d5d3c39e79374e706194cfd1b7f31534d99
9Fingered <sstanton@pswgroup.net>
2025-11-28 22:17:46 -0500

    Compacting c switch machine test passes.

---
+++ test/full_test129/sub2-actions.c
@@ -0,0 +1,39 @@
+
+#include "sub2_priv.h"
+
+ACTION_RETURN_TYPE UFMN(noAction)(FSM_TYPE_PTR pfsm)
+{
+    DBG_PRINTF("%s", __func__);
+    (void) pfsm;
+
+
+    return THIS(noEvent);
+}
+
+void UFMN(grab_parent_e1_data)(pTEST_FSM_DATA pdata, const void *pthis_fsm)
+{
+    DBG_PRINTF("%s", __func__);
+
+    FSM_TYPE_PTR psub2 = (FSM_TYPE_PTR) pthis_fsm;
+
+    psub2->data.sub2_e1_count += pdata->e1_count;
+
+    DBG_PRINTF("Instance: %u: sub2 e1 accumulator: %u"
+               , psub2->instance
+               , psub2->data.sub2_e1_count
+              );
+}
+
+TR_FN_RETURN_TYPE UFMN(toggle)(pSUB2 pfsm,TEST_FSM_EVENT_ENUM event)
+{
+	(void) event;
+
+	DECLARE_TR_FN_RET_VAR(ret, s1);
+
+	if (pfsm->state == STATE(s1))
+	{
+		RETURN_STATE(ret, s2);
+	}
+
+	return ret;
+}
---
+++ test/full_test129/test_fsm-actions.c
@@ -0,0 +1,28 @@
+#include "test_fsm_priv.h"
+
+ACTION_RETURN_TYPE UFMN(noAction)(FSM_TYPE_PTR pfsm)
+{
+    DBG_PRINTF("%s", __func__);
+    (void) pfsm;
+
+
+    return THIS(noEvent);
+}
+
+void test_fsm_grab_e1_data(pTEST_FSM_DATA pfsm_data,pTEST_FSM_E1_DATA pdata)
+{
+    DBG_PRINTF("%s", __func__);
+
+    pfsm_data->e1_count += pdata->i;
+
+    DBG_PRINTF("e1 accumulator: %u", pfsm_data->e1_count);
+
+}
+
+
+void UFMN(record)(pTEST_FSM pfsm, TEST_FSM_STATE new_s)
+{
+    DBG_PRINTF("%s", __func__);
+	 (void) pfsm;
+	 (void) new_s;
+}
