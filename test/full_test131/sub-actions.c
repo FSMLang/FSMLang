61ebc811b82ec34a4b075f7b6616c63e8df64aad
9Fingered <sstanton@pswgroup.net>
2025-12-24 11:08:03 -0500

    Test for generation of switch machine when actions return states.

---
+++ test/full_test131/sub-actions.c
@@ -0,0 +1,36 @@
+
+#include "sub_priv.h"
+
+ACTION_RETURN_TYPE UFMN(noAction)(FSM_TYPE_PTR pfsm)
+{
+    DBG_PRINTF("%s", __func__);
+    (void) pfsm;
+	return STATE(noTransition);
+}
+
+void UFMN(grab_parent_e1_data)(PARENT_DATA_TYPE_PTR pdata, const void *pthis_fsm)
+{
+    DBG_PRINTF("%s", __func__);
+
+    FSM_TYPE_PTR psub = (FSM_TYPE_PTR) pthis_fsm;
+
+    psub->data.sub_e1_count += pdata->e1_count;
+
+    DBG_PRINTF("Instance: %u: sub e1 accumulator: %u"
+               , psub->instance
+               , psub->data.sub_e1_count
+              );
+}
+
+void UFMN(onEntryTo_s1)(pSUB_DATA pdata)
+{
+    DBG_PRINTF("%s", __func__);
+	 (void) pdata;
+}
+
+void UFMN(onExitFrom_s1)(pSUB_DATA pdata)
+{
+    DBG_PRINTF("%s", __func__);
+	 (void) pdata;
+}
+
