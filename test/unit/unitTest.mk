b9d9b146134d956e6cbf6770a90b797f50914d9d
9Fingered <sstanton@pswgroup.net>
2026-01-10 12:35:34 -0500

    Tests for adding included files to dependency list.

--- test/unit/unitTest.mk
+++ test/unit/unitTest.mk
@@ -1,9 +1,11 @@
-override CFLAGS += -Wall -Wpedantic -Wextra
+override CFLAGS += -Wall -Wpedantic -Wextra -DUNIT_TEST
+
 
 VPATH=../../../src
 
 ifdef OUTPUT_DIR
 include $(OUTPUT_DIR)/system.mk
+VPATH=$(OUTPUT_DIR)/../src
 endif
 
 TARGET ?= runtest
@@ -16,22 +18,36 @@ OBJ=$(SRC:.c=.o)
 
 all: $(TARGET)
 
-clean:
-	-@rm -f $(OBJ) test test.out test.result *.d
+ifdef LIB_SRC
+LIB_OBJ=$(LIB_SRC:.c=.o)
+LIB_NAME=fsm
+ADD_LIB=-Wl,--allow-multiple-definition -L. -l$(LIB_NAME) 
 
-test: $(OBJ)
-	gcc -o $@ $(OBJ)
+$(LIB_NAME): $(LIB_OBJ)
+	@$(AR) rcs $(addprefix lib, $@).a $(LIB_OBJ)
+
+endif
+
+clean:
+	-@rm -f $(OBJ) $(LIB_OBJ) $(addprefix lib, $(LIB_NAME)).a test test.out test.result *.d > /dev/null 2>&1
+
+test: $(OBJ) $(LIB_NAME)
+	@gcc -o $@ $(OBJ) $(ADD_LIB)
 
 runtest: test
-	./test > test.out
+	@./test > test.out $(GRAB_STDERR)
 	-@rm -f test.out
 	-@rm -f $(OBJ) test test.out test.result *.d
 
 diff_canonical: test
-	./test > test.out
-	diff test.out test.canonical > test.result
-	-@rm -f test.result test.out
-	-@rm -f $(OBJ) test test.out test.result *.d
+	@./test > test.out $(GRAB_STDERR)
+	@diff test.out test.canonical > test.result
+	-@rm -f $(OBJ) test test.out test.result *.d > /dev/null 2>&1
+	-@rm -f $(LIB_OBJ) $(addprefix lib, $(LIB_NAME)).a > /dev/null 2>&1
 
+ifdef OUTPUT_DIR
+include $(OUTPUT_DIR)/../depends.mk
+else
 include ../../../depends.mk
+endif
 
