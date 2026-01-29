12d19d63e77baec016f61348708a5cc9d2177277
9Fingered <sstanton@pswgroup.net>
2026-01-28 21:21:07 -0500

    Add plantuml tests.  First test shows that guards list their exits only once.

---
+++ test/plantuml/plantuml.mk
@@ -0,0 +1,37 @@
+##########################################
+#
+# common content for plantuml tests
+#
+#
+
+.PHONY: $(TESTS)
+.SECONDEXPANSION:
+
+FSMS = $(shell find . -name "*.fsm" -printf "%P ")
+TESTS = $(basename $(FSMS))
+
+#override FSM_PLANTUML_FLAGS+=
+
+NO_RUNTEST=TRUE
+include $(OUTPUT_DIR)/../test/test.mk
+
+include $(OUTPUT_DIR)/../fsmrules.mk
+
+echotests:
+	@echo $(TESTS)
+
+runtest: $(TESTS) 
+	@echo "all plantuml tests pass"
+
+$(TESTS): $(FSM) $$@.plantuml Makefile
+	@$(DIFF) $@.plantuml $@.plantuml.canonical > $@.result
+	@echo $@ successful
+	@rm -f $@.plantuml $@.result > /dev/null
+      
+clean::
+	-@rm -f *.plantuml > /dev/null
+	-@rm -f *.stderr > /dev/null
+	-@rm -f *.result > /dev/null
+	-@rm -f *.fsmdh  > /dev/null
+
+
