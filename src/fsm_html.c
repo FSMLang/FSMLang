ade199e0fad561ceb5ced9128ab76491b67a1062
9Fingered <sstanton@pswgroup.net>
2026-01-29 21:24:13 -0500

    Print doc comments for state entry and exit functions.

--- src/fsm_html.c
+++ src/fsm_html.c
@@ -865,6 +865,14 @@ static bool print_state_table_state_row(pLIST_ELEMENT pelem, void *data)
 				: pstate->name
 			   );
 		something_printed = true;
+
+		if (psd->entry_fn->docCmnt)
+		{
+			fprintf(pih->fout
+					, "<p>%s</p>\n"
+					, psd->entry_fn->docCmnt
+					);
+		}
 	}
 
 	if (psd->state_flags & sfHasExitFn)
@@ -879,6 +887,14 @@ static bool print_state_table_state_row(pLIST_ELEMENT pelem, void *data)
 				: pstate->name
 			   );
 		something_printed = true;
+
+		if (psd->exit_fn->docCmnt)
+		{
+			fprintf(pih->fout
+					, "<p>%s</p>\n"
+					, psd->exit_fn->docCmnt
+					);
+		}
 	}
 
 	if (psd->state_flags & sfInibitSubMachines)
