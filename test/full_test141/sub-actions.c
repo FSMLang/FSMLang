8548ce9b88693ab840c2bbfd3e7c7705aab50fc0
9Fingered <101585249+9Fingered@users.noreply.github.com>
2026-02-15 14:50:05 -0500

    249 guard functions are not included in weak function output (#261)
    * Test creation of separate files for weak fns.
    
    * Created separate files for weak fns.
    
    * Remove unused function argument.

---
+++ test/full_test141/sub-actions.c
@@ -0,0 +1,18 @@
+
+#include "sub_priv.h"
+
+void UFMN(grab_parent_e1_data)(PARENT_DATA_TYPE_PTR pdata, const void *pthis_fsm)
+{
+    DBG_PRINTF("%s", __func__);
+
+    FSM_TYPE_PTR psub = (FSM_TYPE_PTR) pthis_fsm;
+
+    psub->data.sub_e1_count += pdata->e1_count;
+
+    DBG_PRINTF("Instance: %u: sub e1 accumulator: %u"
+               , psub->instance
+               , psub->data.sub_e1_count
+              );
+}
+
+
