34df05f80479a15c4b3bd498bdf6512be4a569e2
9Fingered <sstanton@pswgroup.net>
2026-02-16 21:24:39 -0500

    Tests for single switch variant.

---
+++ test/full_test146/sub2-actions.c
@@ -0,0 +1,26 @@
+
+#include "sub2_priv.h"
+
+ACTION_RETURN_TYPE UFMN(noAction)(FSM_TYPE_PTR pfsm)
+{
+	DBG_PRINTF("%s", __func__);
+	(void) pfsm;
+
+
+	return STATE(noTransition);
+}
+
+void UFMN(grab_parent_e1_data)(pTEST_FSM_DATA pdata, const void *pthis_fsm)
+{
+	DBG_PRINTF("%s", __func__);
+
+	FSM_TYPE_PTR psub2 = (FSM_TYPE_PTR) pthis_fsm;
+
+	psub2->data.sub2_e1_count += pdata->e1_count;
+
+	DBG_PRINTF("Instance: %u: sub2 e1 accumulator: %u"
+					, psub2->instance
+					, psub2->data.sub2_e1_count
+					);
+}
+
---
+++ test/full_test147/test.c
@@ -0,0 +1,27 @@
+#include "test_fsm.h"
+
+int main(void)
+{
+    TEST_FSM_EVENT e;
+
+    e.event = THIS(e1);
+
+    e.event_data.e1_data.i = 1;
+    run_test_fsm_instance0(&e);
+    run_test_fsm_instance1(&e);
+    run_test_fsm_instance2(&e);
+
+    run_test_fsm_instance0(&e);
+    run_test_fsm_instance1(&e);
+    run_test_fsm_instance2(&e);
+
+    run_test_fsm_instance0(&e);
+    run_test_fsm_instance1(&e);
+    run_test_fsm_instance2(&e);
+
+    run_test_fsm_instance0(&e);
+    run_test_fsm_instance1(&e);
+    run_test_fsm_instance2(&e);
+
+    return 0;
+}
