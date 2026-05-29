14545d32ff4b61f9c39ca63363114f730f39b6e0
9Fingered <sstanton@pswgroup.net>
2026-05-28 21:24:21 -0400

    capture output in case of failures during file listings.

--- fsmrules.mk
+++ fsmrules.mk
@@ -23,11 +23,11 @@ ifeq ($(TARGET),fsm_fail_is_pass)
 CALL_FSM_FAILURE_A_SUCCESS = ; if [ $$? -ne 0 ]; then echo "expected fsm failure; test passes"; true; else echo "did not find an expected fsm failure; test fails"; false; fi
 endif
 
-GENERATED_SRC      = $(shell $(FSM) -M $(FSM_FLAGS) $(FSM_SRC))
-GENERATED_HDR      = $(shell $(FSM) -Mh $(FSM_FLAGS) $(FSM_SRC))
-GENERATED_HTML     = $(shell $(FSM) -th -M $(FSM_HTML_FLAGS) $(FSM_SRC))
-GENERATED_PLANTUML = $(shell $(FSM) -tp -M $(FSM_PLANTUML_FLAGS) $(FSM_SRC))
-GENERATED_RST      = $(shell $(FSM) -M -tr $(FSM_SRC))
+GENERATED_SRC      = $(shell $(FSM) -M $(FSM_FLAGS) $(FSM_SRC) 2>> fsmout)
+GENERATED_HDR      = $(shell $(FSM) -Mh $(FSM_FLAGS) $(FSM_SRC) 2>> fsmout)
+GENERATED_HTML     = $(shell $(FSM) -th -M $(FSM_HTML_FLAGS) $(FSM_SRC) 2>> fsmout)
+GENERATED_PLANTUML = $(shell $(FSM) -tp -M $(FSM_PLANTUML_FLAGS) $(FSM_SRC) 2>> fsmout)
+GENERATED_RST      = $(shell $(FSM) -M -tr $(FSM_SRC) 2>> fsmout)
 
 cleanfsm:
 	@-rm -f $(GENERATED_SRC) 2> /dev/null
@@ -79,15 +79,20 @@ $(SRC): $(FSM_SRC:.fsm=.h)
 	@set -e; $(FSM) -tr -Md $(FSM_RST_FLAGS) $< > $@
 
 ifneq ($(TARGET),fsm_fail_is_pass)
+
 ifneq ($(MAKECMDGOALS),clean)
+
 ifdef FSM_PLANTUML_FLAGS
 -include $(FSM_SRC:.fsm=.fsmdp)
 endif
+
 ifdef FSM_HTML_FLAGS
 -include $(FSM_SRC:.fsm=.fsmdh)
 endif
+
 -include $(FSM_SRC:.fsm=.fsmd)
 -include $(FSM_SRC:.fsm=.fsmdr)
 endif
+
 endif
 
