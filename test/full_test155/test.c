4f9108214b214587c04932203afaf521c446de43
9Fingered <sstanton@pswgroup.net>
2026-09-05 10:24:47 -0400

    New tests and adjusted tests for data translator machines having void returning actions.

---
+++ test/full_test155/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "test_fsm.h"
+
+int main(void)
+{
+	TEST_FSM_EVENT e;
+
+	e.event = THIS(e0);
+	run_test_fsm(&e);
+
+	e.event = THIS(e1);
+	e.event_data.e1_data.i = 1;
+
+	run_test_fsm(&e);
+	run_test_fsm(&e);
+	run_test_fsm(&e);
+	run_test_fsm(&e);
+
+	return 0;
+}
+
