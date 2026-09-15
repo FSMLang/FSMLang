01e6bc076f09397d78d87fc014e2ee0df2fa6e3c
9Fingered <sstanton@pswgroup.net>
2026-09-14 20:08:15 -0400

    Add test for parent with void returning actions having a data translator returning events.

---
+++ test/full_test157/test_fsm-actions.c
@@ -0,0 +1,34 @@
+#include "test_fsm_priv.h"
+
+TEST_FSM_EVENT_ENUM UFMN(a1)(pTEST_FSM pfsm)
+{
+	DBG_PRINTF("%s", __func__);
+	printf("i: %d; j: %d; k: %d; l: %d\n"
+				, pfsm->data.i, pfsm->data.j, pfsm->data.k, pfsm->data.l
+			);
+	return THIS(noEvent);
+}
+
+ACTION_RETURN_TYPE UFMN(xlate_e1)(pTEST_FSM pfsm, pTEST_FSM_E1_DATA pe1_data, TEST_FSM_EVENT_ENUM event)
+{
+	DBG_PRINTF("%s", __func__);
+
+	pfsm->data.pe1_data = pe1_data;
+
+	return run_test_fsm_e1_translator(pfsm, event);
+}
+
+void UFMN(init_s2_implementor)(FSM_TYPE_PTR pfsm)
+{
+	DBG_PRINTF("%s", __func__);
+
+	run_test_fsm_s2_implementor(pfsm, THIS(e0));
+}
+
+void UFMN(init_s3_implementor)(FSM_TYPE_PTR pfsm)
+{
+	DBG_PRINTF("%s", __func__);
+
+	run_test_fsm_s3_implementor(pfsm, THIS(e0));
+}
+
